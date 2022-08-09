5f8d262f41ab707fedf494208bb8e87ae2c0bb4d
Menas Erian <meena5.erian@gmail.com>
2022-08-09 16:03:57 +0400

    Add simple but effective contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,21 +1,14 @@
 import React from 'react';
-// import sections
-import Hero from '../components/sections/Hero';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
-import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
+import Container from '../components/sections/Container';
 
 const Home = () => {
 
   return (
-    <>
-      <Hero className="illustration-section-01" />
-      <FeaturesTiles />
-      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
-      <Testimonial topDivider />
-      <Cta split />
-    </>
+    <Container
+    title = "Contact us"
+    paragraph = "If you have any questions, suggestions, or feedback about Portacode, please feel free to drop us an email at support@portacode.com"
+    >
+    </Container>
   );
 }
 
